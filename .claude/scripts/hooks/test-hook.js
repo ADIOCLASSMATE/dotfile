c7413df608204b61c79090567dff53e61bcf4813
wjx <cqzxwjx@163.com>
2026-04-15 17:10:41 +0000

    chore: remove temporary test-hook.js

--- .claude/scripts/hooks/test-hook.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-let data = '';
-process.stdin.on('data', chunk => data += chunk);
-process.stdin.on('end', () => {
-  process.stderr.write('[TEST HOOK] PreToolUse hook fired! Input: ' + data.slice(0, 100) + '\n');
-  process.stdout.write(data);
-  process.exit(0);
-// hook test edit
-});
